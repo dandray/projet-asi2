135a5860b51014d986a31046e13dbcaa9f15a1be
mouchou <sabbioni.maxime@gmail.com>
2016-12-09 17:48:31 +0100

    Fin step 2 JEE Mouchou

--- JEE/MiddleAuthWatcherEJB/src/main/java/fr/cpe/services/impl/MessageReceiverSync.java
+++ JEE/MiddleAuthWatcherEJB/src/main/java/fr/cpe/services/impl/MessageReceiverSync.java
@@ -7,6 +7,7 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.ObjectMessage;
 import javax.jms.TextMessage;
@@ -19,24 +20,29 @@ import fr.cpe.services.MessageReceiverSyncLocal;
 @LocalBean
 public class MessageReceiverSync implements MessageReceiverSyncLocal {
 	
-	Logger logger = Logger.getLogger(MessageReceiverSyncLocal.class);
+	Logger logger = Logger.getLogger(MessageReceiverSyncLocal.class.getName());
 	@Inject
 	JMSContext context;
 	@Resource(mappedName = "java:/jms/queue/watcherqueue")
 	Topic topic;
 	
 public UserModel receiveMessage() {
-	
+	try {
 	Message message = context.createConsumer(topic).receive(1000);
 		if (message instanceof TextMessage){
-			logger.log(((TextMessage) message).getText());
+			
+				logger.info(((TextMessage) message).getText().toString());
+
 		}
 		else if (message instanceof ObjectMessage){
-			UserModel obj = (UserModel) ((ObjectMessage) message).getObject();
-			return obj;
-		}
-		else {
-			return (UserModel) message;
-		}
+			UserModel obj = null;
+				obj = (UserModel) ((ObjectMessage) message).getObject();
+				return obj;
+			}
+	} catch (JMSException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		return null;
 	}
 }
